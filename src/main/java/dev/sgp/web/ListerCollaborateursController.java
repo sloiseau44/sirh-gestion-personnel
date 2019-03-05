c494af90ca879028afc7fc0ecc8818cccdfffeef
Stéphane Loiseau <stephloiseau@free.fr>
2019-03-05 16:43:22 +0100

    Commit en début de TP3

--- src/main/java/dev/sgp/web/ListerCollaborateursController.java
+++ src/main/java/dev/sgp/web/ListerCollaborateursController.java
@@ -1,6 +1,7 @@
 package dev.sgp.web;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,16 +15,7 @@ public class ListerCollaborateursController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// recupere la valeur d&#39;un parametre dont le nom est avecPhoto
-		String avecPhotoParam = req.getParameter("avecPhoto");
-		// recupere la valeur d&#39;un parametre dont le nom est departement
-		String departementParam = req.getParameter("departement");
-		resp.setContentType("text/html");
-		// code HTML ecrit dans le corps de la reponse
-		resp.getWriter().write("<h1>Liste des collaborateurs</h1>"
-				+ "<ul>"
-				+ "<li>avecPhoto="+ avecPhotoParam + "</li>"
-				+ "<li>departement="+ departementParam + "</li>"
-				+ "</ul>");
+		req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
 	}
 }
